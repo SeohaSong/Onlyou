aa8b9fd8463a6b5ec13902bacb0ea78e61d3dbc9
seohasong <tisutoo@gmail.com>
2019-03-21 19:51:17 +0900

    continue previous work

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -31,16 +31,14 @@ export class AppComponent {
     box.classList.add('loading')
     if (window.innerHeight < window.innerWidth) box.classList.add('widescreen')
     else box.classList.remove('widescreen')
-    setTimeout(() => {
-      if (window.innerHeight < box.clientHeight+128) {
-        if (box.classList.contains('widescreen')) {
-          box.style.width = (window.innerHeight-32)*2+'px'
-        } else {
-          box.style.width = (window.innerHeight-64)/2+'px'
-        }
-      } else box.style.width = '100%'
-      box.classList.remove('loading')
-    }, 500)
+    if (window.innerHeight < box.clientHeight+128) {
+      if (box.classList.contains('widescreen')) {
+        box.style.width = (window.innerHeight-32)*2+'px'
+      } else {
+        box.style.width = (window.innerHeight-64)/2+'px'
+      }
+    } else box.style.width = '100%'
+    setTimeout(() => box.classList.remove('loading'), 200)
   }
 
   upload() {
