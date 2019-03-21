cebea00765abc6108caaab4e2100e10f675ac5da
seohasong <tisutoo@gmail.com>
2019-03-21 13:59:36 +0900

    continue previous work

---
+++ build-client.sh
@@ -0,0 +1,11 @@
+(
+    cd client
+    ng build --base-href /Onlyou/
+    rm -rf $HOME_PATH/SEOHASONG/seohasong.github.io/Onlyou
+    mv dist/client $HOME_PATH/SEOHASONG/seohasong.github.io/Onlyou
+    rm -rf dist
+    cd ..
+    gitgit
+)
+cd $HOME_PATH/SEOHASONG/seohasong.github.io
+gitgit
