31737e8fed679f471164a0f56734f61ba5dff5ff
seohasong <tisutoo@gmail.com>
2019-03-22 17:44:33 +0900

    continue previous work

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
 
   status = 'input'
   input_img: any
